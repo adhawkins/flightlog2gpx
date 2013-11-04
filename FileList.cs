a539d60284a31d5fbe1cd2032f3499dda6019a6b
Andy Hawkins <andy@gently.org.uk>
2013-11-04 21:21:21 +0000

    Only process .flightlog files in source directory

--- FileList.cs
+++ FileList.cs
@@ -13,26 +13,26 @@ namespace flightlog2gpx
         Queue<string> m_Queue = new Queue<string>();
         string m_Directory;
         Thread m_Thread;
-        bool m_Run=false;
+        bool m_Run = false;
 
         public FileList(string Directory)
         {
             m_Directory=Directory;
 
-            m_Run=true;
-            m_Thread=new Thread(new ThreadStart(ScanDirectory));
+            m_Run = true;
+            m_Thread = new Thread(new ThreadStart(ScanDirectory));
             m_Thread.Start();
         }
 
         public void Dispose()
         {
-            m_Run=false;
+            m_Run = false;
             m_Thread.Join();
         }
 
         public void ScanDirectory()
         {
-            foreach (string File in Directory.GetFiles(m_Directory))
+            foreach (string File in Directory.GetFiles(m_Directory, "*.flightlog", SearchOption.TopDirectoryOnly))
             {
                 if (!m_Run)
                     break;
@@ -45,7 +45,7 @@ namespace flightlog2gpx
                         : File.Remove(index, m_Directory.Length);
 
                     while (StripFile[0] == '\\')
-                        StripFile=StripFile.Remove(0, 1);
+                        StripFile = StripFile.Remove(0, 1);
                     
                     Debug.Print("Storing {0}", StripFile);
                     m_Queue.Enqueue(StripFile);
