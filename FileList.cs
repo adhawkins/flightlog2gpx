969d53b96d24730902c94983e38827937ade3c1c
Andy Hawkins <andy@gently.org.uk>
2013-11-05 13:25:47 +0000

    Watch filesystem for new / changed files

--- FileList.cs
+++ FileList.cs
@@ -14,10 +14,18 @@ namespace flightlog2gpx
         string m_Directory;
         Thread m_Thread;
         bool m_Run = false;
+        FileSystemWatcher m_Watcher;
 
         public FileList(string Directory)
         {
             m_Directory=Directory;
+            m_Watcher = new FileSystemWatcher(Directory, "*.flightlog");
+            
+            m_Watcher.Changed +=new FileSystemEventHandler(m_Watcher_Changed);
+            m_Watcher.Created +=new FileSystemEventHandler(m_Watcher_Changed);
+            m_Watcher.Renamed += new RenamedEventHandler(m_Watcher_Renamed);
+
+            m_Watcher.EnableRaisingEvents = true;
 
             m_Run = true;
             m_Thread = new Thread(new ThreadStart(ScanDirectory));
@@ -26,29 +34,56 @@ namespace flightlog2gpx
 
         public void Dispose()
         {
+            m_Watcher.EnableRaisingEvents = false;
+
             m_Run = false;
             m_Thread.Join();
         }
 
-        public void ScanDirectory()
+        void m_Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Debug.Print("In Changed with {0}", e.FullPath);
+            AddFile(e.FullPath);
+        }
+
+        void m_Watcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            Debug.Print("In Renamed with {0}", e.FullPath);
+            AddFile(e.FullPath);
+        }
+
+        private void ScanDirectory()
         {
             foreach (string File in Directory.GetFiles(m_Directory, "*.flightlog", SearchOption.TopDirectoryOnly))
             {
                 if (!m_Run)
                     break;
 
-                lock (m_Queue)
-                {
-                    int index = File.IndexOf(m_Directory);
-                    string StripFile = (index < 0)
-                        ? File
-                        : File.Remove(index, m_Directory.Length);
+                AddFile(File);
+            }
+        }
 
-                    while (StripFile[0] == '\\')
-                        StripFile = StripFile.Remove(0, 1);
-                    
-                    Debug.Print("Storing {0}", StripFile);
+        private void AddFile(string File)
+        {
+            int index = File.IndexOf(m_Directory);
+            string StripFile = (index < 0)
+                ? File
+                : File.Remove(index, m_Directory.Length);
+
+            while (StripFile[0] == '\\')
+                StripFile = StripFile.Remove(0, 1);
+
+            lock (m_Queue)
+            {
+                Debug.Print("Storing {0}", StripFile);
+                if (!m_Queue.Contains(StripFile))
+                {
                     m_Queue.Enqueue(StripFile);
+                    Debug.Print("Stored {0}", StripFile);
+                }
+                else
+                {
+                    Debug.Print("{0} already exists", StripFile);
                 }
             }
         }
